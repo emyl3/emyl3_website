b94f0f9111a3d7be51d6b8524c13d1412e92262e
elisa lee <elisa.my.lee@gmail.com>
2017-01-11 14:43:56 -0600

    add new vendor files

--- build.sh
+++ build.sh
@@ -4,3 +4,19 @@ mkdir -p public/assets/vendors;
 # angular
 cp node_modules/angular/angular.min.js public/assets/vendors;
 cp node_modules/angular/angular.min.js.map public/assets/vendors;
+
+# angular-route
+cp node_modules/angular-route/angular-route.min.js public/assets/vendors;
+cp node_modules/angular-route/angular-route.min.js.map public/assets/vendors;
+
+# angular-animate
+cp node_modules/angular-animate/angular-animate.min.js public/assets/vendors;
+cp node_modules/angular-animate/angular-animate.min.js.map public/assets/vendors;
+
+# bootstrap css
+cp node_modules/bootstrap/dist/css/bootstrap.min.css public/assets/vendors;
+cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/assets/vendors;
+
+# angular-ui-bootstrap
+cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-csp.css public/assets/vendors;
+cp node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js public/assets/vendors;
